ca6a0c32bba2617689baf9e7e4c68e098d4b9b46
Đỗ Tuấn Minh <128591959+Tuanminh2812003@users.noreply.github.com>
2025-01-24 12:10:23 +0700

    tet 6

--- src/Layouts/QTDTTA/Section5/index.js
+++ src/Layouts/QTDTTA/Section5/index.js
@@ -10,7 +10,7 @@ function Section5(){
                         <div className="Section5__inner__row1">
                             <div className="Section5__inner__row1__text">
                                 <div className="Section5__inner__disc">
-                                    Khu danh thắng Tràng An – Tam Cốc – Bích Động: 
+                                    Khu danh thắng Tràng An – Tam Cốc – Bích Động
                                 </div>
                                 <div className="Section5__inner__text2">
                                     Tràng An là một khu vực nổi bật với hệ thống núi đá vôi kỳ vĩ, các hang động và sông nước tạo nên một cảnh quan thơ mộng, hoang sơ. Du khách có thể tham gia đi thuyền ngược dòng sông, qua các hang động thạch nhũ và chiêm ngưỡng phong cảnh núi non nơi này, đồng thời khám phá các di tích lịch sử, như đền thờ, lăng mộ, chùa chiền gắn liền với lịch sử hình thành và phát triển của đất nước.
@@ -18,11 +18,11 @@ function Section5(){
                                 <div className="Section5__inner__text2">
                                     Tam Cốc được ví như “Hạ Long trên cạn" với ba hang động xuyên qua núi mang nhiều hình thù độc đáo đa dạng mà du khách có thể chiêm ngưỡng khi di chuyển bằng thuyền dọc theo con sông Ngô Đồng. Với những ruộng lúa bát ngát vào mùa nước đổ, kết hợp với hình ảnh núi đá vôi trùng điệp và những cánh đồng lúa xanh mướt, Tam Cốc không chỉ có vẻ đẹp thiên nhiên mà còn là nơi lưu giữ những giá trị văn hóa, lịch sử sâu sắc của vùng đất Cố đô Hoa Lư.
                                 </div>
-                                <div className="Section5__inner__text2">
+                                <div className="Section5__inner__text2" style={{marginBottom: "24px"}}>
                                     Bích Động, nằm cách không xa Tam Cốc, được biết đến với những ngôi chùa cổ kính và cảnh quan thiên nhiên tuyệt đẹp. Đặc biệt, Bích Động là nơi có ngôi chùa Bích Động nằm giữa cảnh núi non hùng vĩ, được xây dựng từ thế kỷ 15. Đây là một địa điểm hành hương nổi tiếng và là điểm đến hấp dẫn cho những ai yêu thích sự yên bình và vẻ đẹp tâm linh.
                                 </div>
                                 <div className="Section5__inner__disc">
-                                    Rừng nguyên sinh đặc dụng Hoa Lư:
+                                    Rừng nguyên sinh đặc dụng Hoa Lư
                                 </div>
                                 <div className="Section5__inner__text2">
                                     Rừng nguyên sinh đặc dụng Hoa Lư nằm trong khu vực trung tâm của tỉnh Ninh Bình. Khu rừng này có diện tích rộng lớn, là một phần của hệ thống rừng đặc dụng được bảo vệ nghiêm ngặt nhằm duy trì sự đa dạng sinh học và bảo tồn các loài động thực vật quý hiếm. Đây không chỉ là một kho tàng đa dạng sinh học quý giá mà còn là địa điểm lý tưởng cho những ai yêu thích khám phá thiên nhiên hoang dã và tìm hiểu về hệ sinh thái rừng nhiệt đới.
